4c4a804fca98852f1ccdfcdcfbd12e64b6de5e5c
윤관 <blackgar@naver.com>
2021-08-12 19:36:14 +0900

    210812 algorithm Upload

--- day02/Gwan/체육복.py
+++ day02/Gwan/체육복.py
@@ -8,39 +8,3 @@ def solution(n, lost, reserve):
             lost_del.remove(i+1) 
     return n-len(lost_del)
 
-
-def solution(n, lost, reserve): 
-    for res in reserve: 
-        if res in lost: 
-            lost.remove(res) 
-            reserve.remove(res) 
-    answer = n - len(lost) 
-    for i in lost: 
-        if i in reserve: 
-            answer += 1 
-            reserve.remove(i) 
-            
-        elif i - 1 in reserve: 
-            answer += 1 
-            reserve.remove(i - 1) 
-        
-        elif i + 1 in reserve: 
-            answer += 1 
-            reserve.remove(i + 1) 
-    return answer
-
-
-def solution(n, lost, reserve): 
-    reserve_n = list(set(reserve) - set(lost)) 
-    lost_n = list(set(lost) - set(reserve))
-
-    answer = n - len(lost_n) 
-    for i in lost_n: 
-        if i - 1 in reserve_n: 
-            answer += 1 
-            reserve_n.remove(i - 1) 
-        elif i + 1 in reserve_n: 
-            answer += 1 
-            reserve_n.remove(i + 1) 
-    
-    return answer
